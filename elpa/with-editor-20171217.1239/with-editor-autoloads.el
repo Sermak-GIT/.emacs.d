e29328a0c03fabada28d5a56c44b2c59105569a1
Egidius Mysliwietz <egidius.mysliwietz@gmx.de>
2018-01-09 20:58:31 +0100

    Added Magit

---
+++ elpa/with-editor-20171217.1239/with-editor-autoloads.el
@@ -0,0 +1,16 @@
+;;; with-editor-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
+;;;;;;  (23125 7419 258323 972000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; with-editor-autoloads.el ends here
